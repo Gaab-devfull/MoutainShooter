9d563d94ee1a4fb4d4a4dfa369b8eaf9784f0290
gaab-devfull <gabriel.tecads@gmail.com>
2025-08-29 21:02:32 -0300

    create main loop create Menu and Game classes

--- code/game.py
+++ code/game.py
@@ -1,9 +1,25 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+import pygame
+
+from code.menu import Menu
+
 
 class Game:
     def __init__(self):
-        self.window = None
+        # iniciando o código
+        pygame.init()
+        # criando a janela do pygame e definindo tamanho
+        self.window = pygame.display.set_mode(size=(600, 480))
 
-    def run(self, ):
-        pass
+    def run(self):
+# 'loop' para eventos que guiarão ações da janela
+        while True:
+            menu = Menu(self.window)
+            menu.run()
+            pass
+    # checando cada evento da janela
+         #for event in pygame.event.get():
+             #if event.type == pygame.QUIT:
+                #pygame.quit()  # Para fechar a janela
+                #quit()  # para de fato fechar o pygame
--- main.py
+++ main.py
@@ -1,17 +1,4 @@
-import pygame
+from code.game import Game
 
-print('setup start')
-# iniciando o código
-pygame.init()
-# criando a janela do pygame e definindo tamanho
-window = pygame.display.set_mode(size=(600, 480))
-print('setup end')
-
-# 'loop' para eventos que guiarão ações da janela
-print('loop start')
-while True:
-    # checando cada evento da janela
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()  # Para fechar a janela
-            quit()  # para de fato fechar o pygame
+game = Game()
+game.run()
